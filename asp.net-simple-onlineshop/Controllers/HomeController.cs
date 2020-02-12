977e54f94aeaa2e75d7dbb9f1d7906b84c2edad2
Henry De Guzman <henrydeguzman.java73@gmail.com>
2020-02-12 13:45:52 +0700

    Creating the Home Page

---
+++ asp.net-simple-onlineshop/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using asp.net_simple_onlineshop.Models;
+using asp.net_simple_onlineshop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asp.net_simple_onlineshop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var homeViewModel = new HomeViewModel
+            {
+                PiesOfTheWeek = _pieRepository.PiesOfTheWeek
+            };
+            return View(homeViewModel);
+        }
+    }
+}
---
+++ asp.net-simple-onlineshop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using asp.net_simple_onlineshop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace asp.net_simple_onlineshop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Pie> PiesOfTheWeek { get; set; }
+    }
+}
